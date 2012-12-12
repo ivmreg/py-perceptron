797ba802c117a0ceefd2801ff902a9aa22e1f0aa
Leif Johnson <leif@leifjohnson.net>
2012-12-11 18:07:53 -0600

    Add README.md to package manifest ; update setup.py to reflect best packaging know-how to date.

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
+import os
 import setuptools
 
 setuptools.setup(
     name='lmj.perceptron',
-    version='0.1',
+    version='0.1.1',
     namespace_packages=['lmj'],
     packages=setuptools.find_packages(),
-    ext_modules=[setuptools.Extension('lmj.pursuit._correlate', sources=['lmj/pursuit/correlate.c'])],
     author='Leif Johnson',
     author_email='leif@leifjohnson.net',
-    description='A library of perceptron implementations ',
-    long_description=open('README.md').read(),
+    description='A library of perceptrons',
+    long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')).read(),
     license='MIT',
+    url='http://github.com/lmjohns3/py-perceptron',
     keywords=('perceptron '
               'averaged-perceptron '
               'machine-learning'),
-    url='http://github.com/lmjohns3/py-perceptron',
+    ext_modules=[setuptools.Extension('lmj.pursuit._correlate', sources=['lmj/pursuit/correlate.c'])],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
